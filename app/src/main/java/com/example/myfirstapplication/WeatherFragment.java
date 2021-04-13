5c32b703ca03c0f354f4d1a2757ce28b25c08c43
Frederic Iron <50624465+FreddyTF@users.noreply.github.com>
2021-04-13 12:57:49 +0200

    Passt

--- app/src/main/java/com/example/myfirstapplication/WeatherFragment.java
+++ app/src/main/java/com/example/myfirstapplication/WeatherFragment.java
@@ -9,12 +9,16 @@ import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
-import org.apache.commons.io.IOUtils;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
@@ -43,20 +47,34 @@ public class WeatherFragment extends Fragment {
             try {
                 URLConnection urlConnection = new URL("http://10.0.2.2:8080").openConnection();
                 try (InputStream inputStream = urlConnection.getInputStream()) {
-                    String result = IOUtils.toString(inputStream, "UTF-8");
+                    //String result = IOUtils.toString(inputStream, "UTF-8");
 
+                    Map<String, Object> fromJson = new Gson().fromJson(
+                            new InputStreamReader(inputStream, "UTF-8"),
+                            new TypeToken<HashMap<String, Object>>() {
+                            }.getType()
+                    );
+                    Object temperature = fromJson.get("temperature");
+                    root.post(()->
+                    {
+
+                        TextView textView = root.findViewById(R.id.textView);
+                        textView.setText(temperature.toString());
+
+                    });
                 }
             } catch (IOException e) {
                 e.printStackTrace();
+                root.post(()->
+                {
+
+                    TextView textView = root.findViewById(R.id.textView);
+                    textView.setText(e.getClass().getSimpleName() + ": " + e.getMessage());
+
+                });
             }
 
-            root.post(()->
-            {
 
-                TextView textView = root.findViewById(R.id.textView);
-                textView.setText(city);
-
-            });
 
         });
 
