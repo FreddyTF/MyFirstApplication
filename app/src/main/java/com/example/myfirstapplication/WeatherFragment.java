01c463eb031235416fa9b8e77b8d8677d94acf05
Frederic Iron <50624465+FreddyTF@users.noreply.github.com>
2021-04-13 10:53:30 +0200

    Passt

--- app/src/main/java/com/example/myfirstapplication/WeatherFragment.java
+++ app/src/main/java/com/example/myfirstapplication/WeatherFragment.java
@@ -1,12 +1,16 @@
 package com.example.myfirstapplication;
 
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,18 +19,35 @@ import androidx.fragment.app.Fragment;
  */
 public class WeatherFragment extends Fragment {
 
-
+    Executor executor = Executors.newSingleThreadExecutor();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        String city = getArguments().getString("city");
 
         View root = inflater.inflate(R.layout.fragment_weather, container, false);
 
-        TextView textView = root.findViewById(R.id.textView);
-        textView.setText(city);
+        // Inflate the layout for this fragment
+        executor.execute(()-> {
+
+            SystemClock.sleep(3000);
+            String city = getArguments().getString("city");
+
+            root.post(()->
+            {
+
+                TextView textView = root.findViewById(R.id.textView);
+                textView.setText(city);
+
+            });
+
+        });
+
+
+
+
+
+
         return root;
     }
 }
