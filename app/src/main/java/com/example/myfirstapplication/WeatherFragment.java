3a21b45bb7d6f7567b5d6f966f3e00fa6e15db7e
Frederic Iron <50624465+FreddyTF@users.noreply.github.com>
2021-04-13 12:58:19 +0200

    Passt

--- app/src/main/java/com/example/myfirstapplication/WeatherFragment.java
+++ app/src/main/java/com/example/myfirstapplication/WeatherFragment.java
@@ -1,7 +1,6 @@
 package com.example.myfirstapplication;
 
 import android.os.Bundle;
-import android.os.SystemClock;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -40,7 +39,7 @@ public class WeatherFragment extends Fragment {
         // Inflate the layout for this fragment
         executor.execute(()-> {
 
-            SystemClock.sleep(1000);
+            //SystemClock.sleep(1000);
             String city = getArguments().getString("city");
 
 
